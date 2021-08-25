0f53d90b4523743cfe5eb8132e825d8261fb61a7
Iam-The-Master <71867099+Iam-The-Master@users.noreply.github.com>
2021-08-25 11:46:45 +0000

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,11 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>
-🔸 My Name : @MjFilesBot 
-🔸 Creator : @MasterOfTG  
-🔸 Our Channel : @Mj_Linkz
-🔸 Our Group : @Moviejunction </b>",
+            text = f"<b>🔸 My Name : @MjFilesBot \n🔸 Creator : @MasterOfTG  \n🔸 Our Channel : @Mj_Linkz\n🔸 Our Group : @MovieJunction</b>",
             disable_web_page_preview = True,
             reply_markup=InlineKeyboardMarkup(
                 [
